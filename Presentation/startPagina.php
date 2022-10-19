3f1023868b94df6babe7fa43d6ca6d335a01a7bc
Thijs <thijs.puype@gmail.com>
2022-10-19 13:58:17 +0200

    css artikel pagina

--- Presentation/startPagina.php
+++ Presentation/startPagina.php
@@ -1,5 +1,6 @@
 <?php
-    declare(strict_types=1);
+
+declare(strict_types=1);
 ?>
 <!DOCTYPE html>
 <html lang="nl">
@@ -8,8 +9,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="style.css">
+
     <title>Prularia</title>
 </head>
 
@@ -78,51 +80,56 @@
             <section class="artikelOverzicht">
                 <h1>Aanbevolen producten</h1>
                 <!-- placeholders tijdelijk-->
-                <?php 
-                    $teller = 0;  
-                    foreach ($artikelLijst as $artikel) {
+                <section class="producten clearFix">
+                <?php
+                $teller = 0;
+                foreach ($artikelLijst as $artikel) {
                     $teller++;
                 ?>
-                <article class="artikel">
-                    <img src="img/dummy.avif" alt="" class="productFoto">
-                    <h4><?php print $artikel->getNaam(); ?></h4>
-                    <p>€<?php print $artikel->getPrijs(); ?>
-                    <p>
-                    <?php if ($artikel->isInVoorraad()) { ?>    
-                    <p class="pBeschikbaarheid">In voorraad</p>
-                    <?php }else{ ?> 
-                    <p class="pBeschikbaarheid">Niet beschikbaar</p> 
+                    <article class="artikel">
+                        <img src="img/dummy.avif" alt="" class="productFoto">
+                        <h4><?php print $artikel->getNaam(); ?></h4>
+                        <p>€<?php print $artikel->getPrijs(); ?>
+                        <p>
+                            <?php if ($artikel->isInVoorraad()) { ?>
+                        <p class="pBeschikbaarheid">In voorraad</p>
+                    <?php } else { ?>
+                        <p class="pBeschikbaarheid">Niet beschikbaar</p>
                     <?php } ?>
-                    <form method="post" action="./startPagina.php?action=voegToe&id=<?php print($artikel->getArtikelId());?>" class="winkelKarPerArtikelForm">
+                    <form method="post" action="./startPagina.php?action=voegToe&id=<?php print($artikel->getArtikelId()); ?>" class="winkelKarPerArtikelForm">
                         <input type="number" name="aantalVanArtikel" id="aantalVanArtikel">
                         <button type="submit" class="winkelkarArtikelBtn"><img src="img/winkelkar.png" alt=""></button>
                     </form>
-                </article>&nbsp;&nbsp;&nbsp;&nbsp;
-                <?php 
-                if ($teller === 4) { 
-                    print "<br><br>";
-                    $teller = 0;
-                 }
-                } 
-                ?>      
-                <br>
-                <?php   
-            if ($pagina > 1) {
-                print '<a href="./startPagina.php?page=' . ($pagina - 1) . '">' . "<=" . '</a> '; 
-            }
-            for($paginaT=1;$paginaT<=$aantalPaginas;$paginaT++){
-                if ($pagina === $paginaT) {
-                print '<a href="./startPagina.php?page=' . $paginaT . '">' . $paginaT . '</a> ';
-                }else{
-                print '<a href="./startPagina.php?page=' . $paginaT . '">' . $paginaT . '</a> ';    
+                    </article>
+                    
+                    <?php
+                    if ($teller === 4) {
+                        print "<br><br>";
+                        $teller = 0;
+                    }
                 }
-            }
-            if ($pagina < $aantalPaginas) {
-                print '<a href="./startPagina.php?page=' . ($pagina + 1) . '">' . "=>" . '</a> '; 
-            }
-            ?>
+                    ?>
+                </section>
+                    <br>
+                    <div class="paginaCounter">
+                    <?php
+                    if ($pagina > 1) {
+                        print '<a  href="./startPagina.php?page=' . ($pagina - 1) . '">' . "<=" . '</a> ';
+                    }
+                    for ($paginaT = 1; $paginaT <= $aantalPaginas; $paginaT++) {
+                        if ($pagina === $paginaT) {
+                            print '<a href="./startPagina.php?page=' . $paginaT . '">' . $paginaT . '</a> ';
+                        } else {
+                            print '<a href="./startPagina.php?page=' . $paginaT . '">' . $paginaT . '</a> ';
+                        }
+                    }
+                    if ($pagina < $aantalPaginas) {
+                        print '<a href="./startPagina.php?page=' . ($pagina + 1) . '">' . "=>" . '</a> ';
+                    }
+                    ?>
+                    </div>
             </section>
-             
+
         </div>
     </main>
 
