4907d87bcee78852817e1b16a187155613a2cd6b
Tom Lacor <tomlacor@gmail.com>
2022-10-19 07:57:27 +0200

    kleine aanpassing

--- startPagina.php
+++ startPagina.php
@@ -6,9 +6,9 @@ spl_autoload_register();
 
 session_start();
 
-/*Tom: Ik wist voor de controller uiteraard nog niet wat we hier exact gaan nodig hebben
-dus er is een lijst gegenereerd van de artikelen door islam en ook een mogelijkheid van om van elk
-artikel de score te weten te komen eventueel om ze te rangschikken onderaan, vandaar de foreach.*/
+/*Tom: Hieronder is een lijst gegenereerd van de artikelen door islam met een functie van mij erin 
+om de rating toe te voegen daaronder ook een foreach om artikel objecten te genereren voor moesten we
+die nodig hebben.*/
 
 use Business\ArtikelService;
 use Entities\Artikel;
@@ -33,13 +33,9 @@ if (isset($_GET["page"])) {
 $eerstePaginaArtikel = ($pagina-1)*$aantalArtikelsPerPagina;
 $artikelLijst = $artikelSvc->getArtikelOverzicht((int) $eerstePaginaArtikel, (int) $aantalArtikelsPerPagina);
 
-foreach ($artikelLijst as $artikel) { //elk $artikel = een object $artikel
-    //Hieronder ga ik voor elk artikel het artikelId gaan halen
-               $artikelId = $artikel->getArtikelId();
-               $rating = $artikelSvc->getRating($artikelId);
-               //voor elk artikelId wordt in de database gezocht naar een score
-               //indien score onbestaande is wordt een 0 terug gegeven
-               //Op basis van de scores die eruit gehaald worden zou dan eventueel gerangschikt kunnen worden in de presentation
-            }
+foreach ($artikelLijst as $artikel) { // Tom: objecten artikel hebben we mogelijk nodig
+           
+
+  }
 
 include("Presentation/startPagina.php");
