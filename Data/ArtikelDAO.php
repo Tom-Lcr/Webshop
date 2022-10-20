802a79d8316f4d23ad60c7e7a6803e9093650f94
jens <jens.verstraete@vdabcampus.be>
2022-10-20 11:43:17 +0200

    artikelenMetScores, opties voor volgorde en paginas toegevoegd in artikelDAO

--- Data/ArtikelDAO.php
+++ Data/ArtikelDAO.php
@@ -1,7 +1,10 @@
 <?php
 //Data/ArtikelDAO
 
-declare(strict_types = 1);
+//pagination met limit, zie bvb:
+//https://www.myprogrammingtutorials.com/create-pagination-with-php-and-mysql.html
+
+declare(strict_types=1);
 
 namespace Data;
 
@@ -12,49 +15,66 @@ use Entities\Artikel;
 
 
 
-class ArtikelDAO {
-    
-    //Onderste functie berekent rating op basis van artikelID
-     /*De query haalt normaal gesproken het getal uit de database dat overeenkomt met hun score (som/aantal)*/
+class ArtikelDAO
+{
 
-    public function getRatingByArtikelId(int $artikelId) :? float {
-        $sql = "select bestellijnen.artikelId as artikelId, (sum(score)/count(score)) as rating from bestellijnen, 
-        klantenreviews where bestellijnen.bestellijnId = klantenreviews.bestellijnId and artikelId = :artikelId";
-	$dbh = new PDO(DBConfig::$DB_CONNSTRING, DBConfig::$DB_USERNAME, DBConfig::$DB_PASSWORD);
-	$stmt = $dbh->prepare($sql);
-    $stmt->execute(array(':artikelId' => $artikelId));
-	$rij = $stmt->fetch(PDO::FETCH_ASSOC);
-	$rating = floatval($rij["rating"]);
-	$dbh = null;
-	return $rating;
+  
+
+    private string $metScores = "WITH scores(rating, id) AS (SELECT AVG(score), artikelId FROM klantenreviews inner JOIN bestellijnen on klantenreviews.bestellijnId = bestellijnen.bestellijnId GROUP BY artikelId), 
+    artikelenMetScores AS (SELECT artikelId, ean, naam, beschrijving, prijs, gewichtInGram, voorraad, levertijd, rating FROM artikelen LEFT OUTER JOIN scores on artikelen.artikelId = scores.id)";
+    public function metPaginas(string $sql, int $pagina, int $aantal = 20): string
+    {
+        return $sql . " LIMIT " . (int) ($pagina - 1) * $aantal . ", " . $aantal;
     }
 
-    public function getAll(int $waarde1, int $waarde2): array {
-        $optelwaarde = $waarde1 + $waarde2;
-        $dbh = new PDO(DBConfig::$DB_CONNSTRING,DBConfig::$DB_USERNAME, DBConfig::$DB_PASSWORD);
-        $statement = $dbh->prepare("select artikelId, ean, naam, beschrijving, prijs, gewichtInGram, voorraad, levertijd from artikelen where artikelId > :wrd1 and artikelId <= :wrd2");
-        $statement->bindValue(":wrd1", $waarde1);
-        $statement->bindValue(":wrd2", $optelwaarde);
+    public function getRatingByArtikelId(int $artikelId): ?float
+    {
+        $sql = "select bestellijnen.artikelId as artikelId, (sum(score)/count(score)) as rating from bestellijnen, 
+        klantenreviews where bestellijnen.bestellijnId = klantenreviews.bestellijnId and artikelId = :artikelId";
+        $dbh = new PDO(DBConfig::$DB_CONNSTRING, DBConfig::$DB_USERNAME, DBConfig::$DB_PASSWORD);
+        $stmt = $dbh->prepare($sql);
+        $stmt->execute(array(':artikelId' => $artikelId));
+        $rij = $stmt->fetch(PDO::FETCH_ASSOC);
+        $rating = floatval($rij["rating"]);
+        $dbh = null;
+        return $rating;
+    }
+
+    public function getAll(int $pagina = 0, int $aantalPerPagina = 20, string $volgorde = "rating DESC, prijs DESC"): array
+    {
+        
+        $dbh = new PDO(DBConfig::$DB_CONNSTRING, DBConfig::$DB_USERNAME, DBConfig::$DB_PASSWORD);        
+        $statement = $dbh->prepare($this->metScores . "select * from artikelenMetScores ORDER BY $volgorde LIMIT ". $pagina*$aantalPerPagina . ", ". $aantalPerPagina);
+        
+        //$statement->bindValue(":volgorde", $volgorde);
+        
         $statement->execute();
-        $resultSet = $statement->fetchAll(PDO::FETCH_ASSOC);
-        $lijstMetAantal = array();
+        $resultSet = $statement->fetchAll(PDO::FETCH_ASSOC);        
         $lijst = array();
-        foreach($resultSet as $rij){
-            $artikeldao = new ArtikelDAO;
-            $rating = $artikeldao->getRatingByArtikelId((int)$rij["artikelId"]);
-            $artikel = new Artikel((int) $rij["artikelId"], $rij["ean"], $rij["naam"], $rij["beschrijving"], (float) $rij["prijs"], 
-            (int) $rij["gewichtInGram"], (int) $rij["voorraad"], (int) $rij["levertijd"], $rating);
+        foreach ($resultSet as $rij) {                
+            $artikel = new Artikel(
+                (int) $rij["artikelId"],
+                $rij["ean"],
+                $rij["naam"],
+                $rij["beschrijving"],
+                (float) $rij["prijs"],
+                (int) $rij["gewichtInGram"],
+                (int) $rij["voorraad"],
+                (int) $rij["levertijd"],
+                (int) $rij["rating"]
+            );
             array_push($lijst, $artikel);
         }
         $dbh = null;
         return $lijst;
     }
-     
-   
 
-    public function getAantalArtikelRijen(): int {
+
+
+    public function getAantalArtikelRijen(): int
+    {
         $sql = "select * from artikelen";
-        $dbh = new PDO(DBConfig::$DB_CONNSTRING,DBConfig::$DB_USERNAME, DBConfig::$DB_PASSWORD);
+        $dbh = new PDO(DBConfig::$DB_CONNSTRING, DBConfig::$DB_USERNAME, DBConfig::$DB_PASSWORD);
         $resultSet = $dbh->query($sql);
         $aantalRijen = $resultSet->rowCount();
         $dbh = null;
@@ -108,6 +128,4 @@ class ArtikelDAO {
         $dbh = null;
         return $lijst;
     }
-
 }
-
