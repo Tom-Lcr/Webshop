04cc8a8997832fe332286ecfe5fc2a10059237b5
Tom Lacor <tomlacor@gmail.com>
2022-10-25 14:43:32 +0200

    bestellijndao aangevuld

--- Data/BestellijnDAO.php
+++ Data/BestellijnDAO.php
@@ -12,8 +12,23 @@ use Entities\Bestellijn;
 class BestellijnDAO {
     
     
-
+    public function getBestellijnenByBestelId(int $bestelId) : array  {
+        $sql = "select * from bestellijnen where bestelId = :bestelId;";
+        $dbh = new PDO(DBConfig::$DB_CONNSTRING, DBConfig::$DB_USERNAME, DBConfig::$DB_PASSWORD);
+        $stmt = $dbh->prepare($sql);
+        $stmt->execute(array(':bestelId' => $bestelId));
+        $resultset = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $bestellijnen = array();
+        foreach($resultset as $rij)	{
+            $bestellijn = new Bestellijn((int)$rij["bestellijnId"], (int)$rij["bestelId"], (int)$rij["artikelId"], 
+            (int)$rij["aantalBesteldId"], (int)$rij["aantalGeannuleerd"]);
+            array_push($bestellijnen, $bestellijn);
+        }
+        $dbh = null;
+        return $bestellijnen;
+        }
 
    
 
 }
+
