b34bf893b8210014b676aa1b1b0055b12767ad1c
Tom Lacor <tomlacor@gmail.com>
2022-10-26 15:31:41 +0200

    foutmelding eruit

--- Data/ArtikelDAO.php
+++ Data/ArtikelDAO.php
@@ -93,7 +93,7 @@ class ArtikelDAO
         $sql = $this->metScores . "select * from artikelenMetScores";
         $dbh = new PDO(DBConfig::$DB_CONNSTRING, DBConfig::$DB_USERNAME, DBConfig::$DB_PASSWORD);
         $resultSet = $dbh->query($sql);
-        $aantalRijen = /*$resultSet->rowCount();*/ count($resultset);
+        $aantalRijen = $resultSet->rowCount(); 
         $dbh = null;
         return $aantalRijen;
     }
