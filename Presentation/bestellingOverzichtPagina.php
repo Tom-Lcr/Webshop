c1d5f68f4cfbb1d6b3f31d311ce43a654c0614c5
Tom Lacor <tomlacor@gmail.com>
2022-10-26 14:03:43 +0200

    presentatieveranderingen

--- Presentation/bestellingOverzichtPagina.php
+++ Presentation/bestellingOverzichtPagina.php
@@ -25,29 +25,32 @@ declare(strict_types=1);
                     ?>
                             <h3 class="datum"><?php print $bestelling->getBesteldatum(); ?></h3>
                             <h5>Facturatieadres:</h5><br>
-                            <p>Straat:<?php print $bestelling->getFacturatieAdres()->getStraat(); ?></p>
-                            <p>Huisnummer:<?php print $bestelling->getFacturatieAdres()->getHuisNummer(); ?></p>
-                            <p>Postcode:<?php print $bestelling->getFacturatieAdres()->getPlaats()->getPostcode(); ?></p>
-                            <p>Plaats:<?php print $bestelling->getFacturatieAdres()->getPlaats()->getPlaatsNaam(); ?></p>
+                            <p>Straat: <?php print $bestelling->getFacturatieAdres()->getStraat(); ?></p>
+                            <p>Huisnummer: <?php print $bestelling->getFacturatieAdres()->getHuisNummer(); ?></p>
+                            <p>Postcode: <?php print $bestelling->getFacturatieAdres()->getPlaats()->getPostcode(); ?></p>
+                            <p>Plaats: <?php print $bestelling->getFacturatieAdres()->getPlaats()->getPlaatsNaam(); ?></p>
                             <br>
                             <h5>Leveradres:</h5><br>
-                            <p>Straat:<?php print $bestelling->getLeveringsAdres()->getStraat(); ?></p>
-                            <p>Huisnummer:<?php print $bestelling->getLeveringsAdres()->getHuisnummer(); ?></p>
-                            <p>Postcode:<?php print $bestelling->getLeveringsAdres()->getPlaats()->getPostcode(); ?></p>
-                            <p>Plaats:<?php print $bestelling->getLeveringsAdres()->getPlaats()->getPlaatsNaam(); ?></p>
+                            <p>Straat: <?php print $bestelling->getLeveringsAdres()->getStraat(); ?></p>
+                            <p>Huisnummer: <?php print $bestelling->getLeveringsAdres()->getHuisnummer(); ?></p>
+                            <p>Postcode: <?php print $bestelling->getLeveringsAdres()->getPlaats()->getPostcode(); ?></p>
+                            <p>Plaats: <?php print $bestelling->getLeveringsAdres()->getPlaats()->getPlaatsNaam(); ?></p>
                             <br>
-                            <h5>Betaald:</h5>
+                            <h5>Betaald:</h5><br> 
                             <?php if ($bestelling->getBetaald() === 1) {?>
                                 <p>Ja</p>
                             <?php } else {?>
                                 <p>Nee</p>
-                            <?php } ?>   
+                            <?php } ?>  
                             <br>
-                            <h5>Betaalwijze:</h5><p><?php print $bestelling->getBetaalwijze(); ?></p>
+                            <h5>Betaalwijze:</h5><br>
+                            <p><?php print $bestelling->getBetaalwijze(); ?></p>
                             <br>
-                            <h5>Bestellingsstatus:</h5><p><?php print $bestelling->getBestellingsStatus(); ?></p>
+                            <h5>Bestellingsstatus:</h5><br>
+                            <p><?php print $bestelling->getBestellingsStatus(); ?></p>
                             <?php } ?>
-                            <h5>Artikeloverzicht:</h5>
+                            <br>
+                            <h5>Artikeloverzicht:</h5><br>
                             <?php
                               foreach ($bestellijnen as $bestellijn) {
                              ?>
