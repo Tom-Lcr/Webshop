e299178f4228fb6d5ed78c807806c1e4afdb9895
Tom Lacor <tomlacor@gmail.com>
2022-10-26 15:47:04 +0200

    mijn object toegevoegd

--- Data/BestellingDAO.php
+++ Data/BestellingDAO.php
@@ -4,12 +4,16 @@ declare(strict_types = 1);
 
 namespace Data;
 
-use \PDO;
-use Data\DBConfig;
-use Data\BestellijnDAO;
-use DateTime;
+
+
 use Entities\ActieCode;
 use Entities\Bestellijn;
+use DateTime;
+use \PDO;
+use Data\DBConfig;
+use Entities\Bestelling;
+use Data\BestellijnDAO;
+use Data\AdresDAO;
 use Exceptions\ActieCodeBestaatNietException;
 use Exceptions\ActieCodeNietMeerGeldigException;
 use Exceptions\DatabaseException;
@@ -61,4 +65,69 @@ class BestellingDAO {
             $dbh->rollBack();
         }
     }
+    
+    public function getBestellingenByKlantId(int $klantId) : array {
+        $sql = "select * from bestellingen where klantId = :klantId;";
+        $dbh = new PDO(DBConfig::$DB_CONNSTRING, DBConfig::$DB_USERNAME, DBConfig::$DB_PASSWORD);
+        $stmt = $dbh->prepare($sql);
+        $stmt->execute(array(':klantId' => $klantId));
+        $resultSet = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $lijst = array();
+        foreach($resultSet as $rij){
+            $besteldatum = new DateTime($rij["besteldatum"]);
+            $bestellijndao = new BestellijnDAO;
+            $bestellijnen = $bestellijndao->getBestellijnenByBestelId((int)$rij["bestelId"]);
+            $adresdao = new AdresDAO;
+            $facturatieadres = $adresdao->getAdresByAdresId((int)$rij["facturatieAdresId"]);
+            $leveringadres = $adresdao->getAdresByAdresId((int)$rij["leveringsAdresId"]); 
+            $betaalwijzeId = intval($rij["betaalwijzeId"]);
+            if ($betaalwijzeId === 1) {
+                $betaalwijze = "Kredietkaart";
+            } else {
+                $betaalwijze = "Overschrijving";
+            }
+            $bestellingsStatusId = intval($rij["bestellingsStatusId"]);
+            switch ($bestellingsStatusId) {
+                case 1:
+                    $bestellingsStatus = "Lopend";
+                    break;
+                case 2:
+                    $bestellingsStatus = "Betaald";
+                    break;
+                case 3:
+                    $bestellingsStatus = "Geannuleerd";
+                    break;
+                case 4:
+                    $bestellingsStatus = "Klaarmaken";
+                    break;
+                case 5:
+                    $bestellingsStatus = "Onderweg";
+                    break;
+                case 6:
+                    $bestellingsStatus = "Geleverd";
+                    break;
+                case 7:
+                    $bestellingsStatus = "Verloren";
+                    break;
+                case 8:
+                    $bestellingsStatus = "Beschadigd";
+                    break;
+                case 9:
+                    $bestellingsStatus = "Retour";
+                    break;
+                case 10:
+                    $bestellingsStatus = "Retour in stock";
+                    break;                                         
+            }
+            $bestelling = new Bestelling((int)$rij["bestelId"], $besteldatum, (int)$rij["klantId"], 
+            (bool)$rij["betaald"], $rij["betalingscode"], $betaalwijze, (bool)$rij["annulatie"], $rij["terugbetalingscode"],
+            $bestellingsStatus, (bool)$rij["actiecodeGebruikt"], $rij["bedrijfsnaam"], $rij["btwNummer"], $rij["voornaam"],
+            $rij["familienaam"], $facturatieadres, $leveringadres, $bestellijnen);
+            array_push($lijst, $bestelling);
+        }		
+        $dbh = null;
+        return $lijst;
+        }
+    
+   
 }
