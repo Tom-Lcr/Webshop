2ac47ece6a4e1b16a89e8b35a7b95397498e42d2
Tom Lacor <tomlacor@gmail.com>
2022-12-14 17:48:55 +0100

    changes

--- Data/DBConfig.php
+++ Data/DBConfig.php
@@ -10,7 +10,7 @@ namespace Data;
 
 class DBConfig
 {
-    public static $DB_CONNSTRING  = "mysql:host=localhost;dbname=prulariacom;charset=utf8";
+    public static $DB_CONNSTRING  = "mysql:host=localhost;dbname=prularia;charset=utf8";
     public static $DB_USERNAME  = "root";
     public static $DB_PASSWORD = "";
 }
